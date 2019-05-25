3aa742f02d47bb06980c3ffb31ea0d119ef223d6
oskar <oskar@oskar.pl>
2019-05-25 17:10:09 +0200

    Added validation

--- src/main/java/pl/oskarpolak/ministack/controller/AuthController.java
+++ src/main/java/pl/oskarpolak/ministack/controller/AuthController.java
@@ -3,12 +3,15 @@ package pl.oskarpolak.ministack.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.oskarpolak.ministack.model.form.LoginForm;
 import pl.oskarpolak.ministack.model.form.RegisterForm;
 import pl.oskarpolak.ministack.model.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
@@ -23,8 +26,13 @@ public class AuthController {
     }
 
     @PostMapping("/user/register")
-    public String register(@ModelAttribute("registerForm") RegisterForm registerForm,
+    public String register(@ModelAttribute("registerForm") @Valid RegisterForm registerForm,
+                           BindingResult bindingResult,
                            Model model) {
+        if(bindingResult.hasErrors()){
+            return "user/register";
+        }
+
         boolean isRegistered = userService.registerUser(registerForm);
         if(isRegistered){
             return "redirect:/user/login";
--- src/main/java/pl/oskarpolak/ministack/model/form/RegisterForm.java
+++ src/main/java/pl/oskarpolak/ministack/model/form/RegisterForm.java
@@ -2,9 +2,15 @@ package pl.oskarpolak.ministack.model.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class RegisterForm {
+    @Pattern(regexp = ".{5,30}", message = "asdasdasdasdasdasd" )
     private String nickname;
+    @Pattern(regexp = ".+@.+\\..{2,5}")
     private String email;
+    @Size(min = 6, max = 30)
     private String password;
 }
