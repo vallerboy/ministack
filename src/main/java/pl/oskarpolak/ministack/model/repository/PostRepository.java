12a030eb2eab5a9db614b53852c46fcaab260f4a
oskar <oskar@oskar.pl>
2019-05-26 17:08:56 +0200

    Added new add post template

--- src/main/java/pl/oskarpolak/ministack/model/repository/PostRepository.java
+++ src/main/java/pl/oskarpolak/ministack/model/repository/PostRepository.java
@@ -1,11 +1,16 @@
 package pl.oskarpolak.ministack.model.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.oskarpolak.ministack.model.entity.PostEntity;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends CrudRepository<PostEntity, Integer> {
     Iterable<PostEntity> findTop10ByOrderByIdDesc();
 
+    @Query(nativeQuery = true, value = "SELECT * FROM post WHERE user_id = ?1")
+    List<PostEntity> findAllUserPost(int userId);
 }
